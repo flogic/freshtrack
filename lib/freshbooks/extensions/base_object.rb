721462d629bca9e010508f30a4c09359adda7c54
Yossef Mendelssohn <ymendel@pobox.com>
2008-01-30 17:06:56 -0600

    Modified BaseObject#to_xml to not product an error with TimeEntry ( => 'timeentry', when it should be 'time_entry').

--- lib/freshbooks/extensions/base_object.rb
+++ lib/freshbooks/extensions/base_object.rb
@@ -3,5 +3,29 @@ require 'date'
 module FreshBooks
   class BaseObject
     MAPPING_FNS[Date] = lambda { |xml_val|  Date.parse(xml_val.text) }
+    
+    def to_xml
+      # The root element is the elem name
+      root = Element.new elem_name
+      
+      # Add each BaseObject member to the root elem
+      self.members.each do |field_name|
+        
+        value = self.send(field_name)
+        
+        if value.is_a?(Array)
+          node = root.add_element(field_name)
+          value.each { |array_elem| node.add_element(array_elem.to_xml) }
+        elsif !value.nil?
+          root.add_element(field_name).text = value
+        end
+      end
+      root
+    end
+    
+    # The root element is the class name, downcased (and underscored if there is any CamelCase)
+    def elem_name
+      elem_name = self.class.to_s.split('::').last.gsub(/([a-z])([A-Z])/, '\1_\2').downcase
+    end
   end
 end
--- spec/base_object_spec.rb
+++ spec/base_object_spec.rb
@@ -1,5 +1,8 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
+Thing     = FreshBooks::BaseObject.new(:attr)
+ThingDeal = FreshBooks::BaseObject.new(:attr)
+
 describe FreshBooks::BaseObject do
   it 'should have a mapping function for Date' do
     FreshBooks::BaseObject::MAPPING_FNS[Date].should respond_to(:call)
@@ -17,6 +20,32 @@ describe FreshBooks::BaseObject do
     
     it 'should return a date matching the text of its argument' do
       @func.call(@date).to_s.should == @date.text
+    end
+  end
+  
+  describe 'converting an instance to XML' do
+    before :each do
+      @thing = Thing.new
+    end
+    
+    it 'should use the elem name' do
+      @thing.expects(:elem_name)
+      @thing.to_xml
+    end
+  end
+  
+  describe 'getting the elem name' do
+    before :each do
+      @thing = Thing.new
+      @thing_deal = ThingDeal.new
+    end
+    
+    it 'should be the class name, downcased' do
+      @thing.elem_name.should == 'thing'
+    end
+    
+    it 'should be underscored if the class name has CamelCase' do
+      @thing_deal.elem_name.should == 'thing_deal'
     end
   end
 end
