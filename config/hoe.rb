8dc895a27758d2ffa87179544df0ff0e33a26bbe
Yossef Mendelssohn <ymendel@pobox.com>
2008-02-27 15:27:26 -0600

    Modified gemspec to require the 2.1 API version of freshbooks gem.

--- config/hoe.rb
+++ config/hoe.rb
@@ -60,7 +60,7 @@ hoe = Hoe.new(GEM_NAME, VERS) do |p|
   # == Optional
   p.changes = p.paragraphs_of("History.txt", 0..1).join("\n\n")
   p.extra_deps = [     # An array of rubygem dependencies [name, version], e.g. [ ['active_support', '>= 1.3.1'] ]
-    ['freshbooks', '>= 1.0.0'],
+    ['freshbooks', '>= 2.1'],
     ['punch',      '>= 0.0.1']
   ]
   
