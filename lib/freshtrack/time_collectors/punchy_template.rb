4755496f6cc845c5031316420a1e84541689375a
Yossef Mendelssohn <ymendel@pobox.com>
2012-04-11 16:20:34 -0400

    Ignoring still-punched-in times.
    Don't bother tracking something until it's done.
    And don't get an error by trying.

--- lib/freshtrack/time_collectors/punchy_template.rb
+++ lib/freshtrack/time_collectors/punchy_template.rb
@@ -2,26 +2,29 @@ module Freshtrack
   module TimeCollector
     module PunchyTemplate
       attr_reader :options
-      
+
       def initialize(options = {})
         @options = options
       end
-      
+
       def condense_time_data(time_data)
         date_data = times_to_dates(time_data)
         group_date_data(date_data)
       end
-      
+
       def times_to_dates(time_data)
-        time_data.each do |td|
+        time_data.collect do |td|
           punch_in  = td.delete('in')
           punch_out = td.delete('out')
 
-          td['date']  = punch_in.to_date
-          td['hours'] = (punch_out - punch_in).secs_to_hours
-        end
+          if punch_out
+            td['date']  = punch_in.to_date
+            td['hours'] = (punch_out - punch_in).secs_to_hours
+            td
+          end
+        end.compact
       end
-      
+
       def group_date_data(date_data)
         separator = '-' * 20
         grouped = date_data.group_by { |x|  x['date'] }
